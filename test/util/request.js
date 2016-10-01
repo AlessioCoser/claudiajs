80b0ba5aa7fac11b20d8de40079ba05bc2aa0b44
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2016-10-01 15:24:32 +0200

    removes useless sinon dependency

--- test/util/request.js
+++ test/util/request.js
@@ -1,24 +1,21 @@
 var proxyRouter = require.main.require(process.env.npm_package_main).proxyRouter
   , querystring = require('querystring')
-  , spy = require('sinon').spy
 
 module.exports.get = function(path){
-  var context
-    , slices = path.split(/\?/g)
+  var slices = path.split(/\?/g)
     , resourcePath = slices.shift()
     , queryStringParameters = querystring.parse(slices.pop())
+  var err
+    , res
+    , context = {done: function(e, r){ return err = e, res = r}}
   return proxyRouter({
     requestContext: {
       resourcePath: resourcePath,
       httpMethod: 'GET'
     },
     queryStringParameters: queryStringParameters
-  }, context = {
-    done: spy()
-  }).then(function(){
-    if (context.done.lastCall.args[0]) // err
-      return Promise.reject(context.done.lastCall.args[0])
-    return context.done.lastCall.args[1] // res
+  }, context).then(function(){
+    return err ? Promise.reject(err) : Promise.resolve(res)
   })
 }
 
