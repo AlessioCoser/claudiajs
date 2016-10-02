8b63e039ffd7879740e711850ea18dd9f3f70e1e
alessiocoser <alessio.coser@xpeppers.com>
2016-10-02 21:55:58 +0200

    refactors test names

--- test/hello.js
+++ test/hello.js
@@ -3,10 +3,10 @@ describe('/hello', function(){
   var equal = require('assert').equal
   var get = require('./util/request').get
 
-  it('handles facebook auth token', function(){
-    return get('/hello?name=12345').then(function(res){
+  it('print personalized hello message', function(){
+    return get('/hello?name=John').then(function(res){
       equal(res.statusCode, 200)
-      equal(res.body, 'hello <strong>12345</strong>')
+      equal(res.body, 'hello <strong>John</strong>')
     })
   })
 })
